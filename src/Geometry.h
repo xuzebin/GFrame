bba8bde19a6d15c2bc5dd4ae69771762bc51e9f0
xuzebin <zebinxu7@gmail.com>
2016-11-03 11:43:23 -0400

    * Add obj loader

--- src/Geometry.h
+++ src/Geometry.h
@@ -9,8 +9,11 @@
 #ifndef Geometry_h
 #define Geometry_h
 
+#define TINYOBJLOADER_IMPLEMENTATION
+
 #include "geometrymaker.h"
 #include "cvec.h"
+#include "tiny_obj_loader.h"
 
 
 struct VertexPN {
@@ -58,7 +61,7 @@ public:
     virtual void createVBOs() = 0;
     
     void createVBOs(std::vector<VertexPN> vtx, std::vector<unsigned short> idx) {
-        
+        indicesNum = (int)idx.size();
         glGenBuffers(1, &vertexVBO);
         glBindBuffer(GL_ARRAY_BUFFER, vertexVBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(VertexPN) * vtx.size(), vtx.data(), GL_STATIC_DRAW);
@@ -104,8 +107,6 @@ public:
         int vertexBufferLen, indexBufferLen;
         getCubeVbIbLen(vertexBufferLen, indexBufferLen);
         
-        indicesNum = indexBufferLen;
-        
         std::vector<VertexPN> vtx(vertexBufferLen);
         std::vector<unsigned short> idx(indexBufferLen);
         
@@ -131,8 +132,6 @@ public:
     void createVBOs() {
         int vertexBufferLen, indexBufferLen;
         getPlaneVbIbLen(vertexBufferLen, indexBufferLen);
-        
-        indicesNum = indexBufferLen;
         
         std::vector<VertexPN> vtx(vertexBufferLen);
         std::vector<unsigned short> idx(indexBufferLen);
@@ -165,12 +164,8 @@ public:
         int vertexBufferLen, indexBufferLen;
         getSphereVbIbLen(slices, stacks, vertexBufferLen, indexBufferLen);
         
-        
-        indicesNum = indexBufferLen;
-        
         std::vector<VertexPN> vtx(vertexBufferLen);
         std::vector<unsigned short> idx(indexBufferLen);
-        
         
         makeSphere(radius, slices, stacks, vtx.begin(), idx.begin());
         
@@ -193,8 +188,6 @@ public:
         int vertexBufferLen, indexBufferLen;
         getCubeVbIbLen(vertexBufferLen, indexBufferLen);
         
-        indicesNum = indexBufferLen;
-        
         std::vector<VertexPN> vtx(vertexBufferLen);
         std::vector<unsigned short> idx(indexBufferLen);
         
@@ -206,6 +199,52 @@ public:
         }
         
         Geometry::createVBOs(vtx, idx);
+    }
+};
+
+
+class Model : public Geometry {
+private:
+    const std::string fileName;
+public:
+    Model(const std::string fileName_) : fileName(fileName_) {}
+    
+    void createVBOs() {
+        std::vector<VertexPN> vertices;
+        std::vector<unsigned short> indices;
+        loadFromFile(fileName, vertices, indices);
+        
+        Geometry::createVBOs(vertices, indices);
+    }
+    
+private:
+    
+    void loadFromFile(const std::string& fileName, std::vector<VertexPN>& vertices, std::vector<unsigned short>& indices) {
+        tinyobj::attrib_t attrib;
+        std::vector<tinyobj::shape_t> shapes;
+        std::vector<tinyobj::material_t> materials;
+        std::string err;
+        
+        bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &err, fileName.c_str(), NULL, true);
+        
+        if (ret) {
+            for (int i = 0; i < attrib.vertices.size(); i += 3) {
+                VertexPN v;
+                for (int j = 0; j < 3; ++j) {
+                    v.position[j] = attrib.vertices[i + j];
+                    v.normal[j]   = attrib.normals[i + j];
+                }
+                vertices.push_back(v);
+            }
+            for (int i = 0; i < shapes.size(); ++i) {
+                for (int j = 0; j < shapes[i].mesh.indices.size(); ++j) {
+                    indices.push_back(shapes[i].mesh.indices[j].vertex_index);
+                }
+            }
+        } else {
+            std::cout << err << std::endl;
+            assert(false);
+        }
     }
 };
 
