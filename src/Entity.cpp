e0c183800cd64fcf9d52941f5da450a84a7f0519
xuzebin <zebinxu7@gmail.com>
2016-11-19 20:41:46 -0500

    * separate Entity class

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -7,3 +7,162 @@
 //
 
 #include "Entity.hpp"
+
+int Entity::id_counter = 0;
+
+void Entity::createMesh()  {
+    if (geometry == NULL) {
+        throw string("Geometry NULL");
+    }
+    geometry->createVBOs();
+}
+
+void Entity::draw(Camera* camera, ShaderProgram* shaderProgram) {
+    if (!isVisible()) {
+        return;
+    }
+    if (material == NULL) {
+        throw string("Material NULL");
+    }
+    if (geometry == NULL) {
+        throw std::string("Geometry NULL");
+    }
+
+    Matrix4 projectionMatrix = camera->getProjectionMatrix();
+    
+    //Transform hierachy, iteratively multiply parent rigidbody matrices
+    //to get the ultimate modelmatrix that transform from object frame to world frame.
+    Matrix4 modelMatrix;
+    modelMatrix = transform.getModelMatrix();
+    
+    Entity* current = parent;
+    while (current != NULL) {
+        modelMatrix = current->transform.getRigidBodyMatrix() * modelMatrix;
+        current = current->parent;
+    }
+    
+    
+    Matrix4 modelViewMatrix = inv(camera->getViewMatrix()) * modelMatrix;
+    Matrix4 normal = normalMatrix(modelViewMatrix);
+    
+    modelMatrix.writeToColumnMajorMatrix(modelMat);
+    modelViewMatrix.writeToColumnMajorMatrix(modelViewMat);
+    projectionMatrix.writeToColumnMajorMatrix(projectionMat);
+    normal.writeToColumnMajorMatrix(normalMat);
+    
+    glUniformMatrix4fv(shaderProgram->uModelMatrixLoc, 1, false, modelMat);
+    glUniformMatrix4fv(shaderProgram->uModelViewMatrixLoc, 1, false, modelViewMat);
+    glUniformMatrix4fv(shaderProgram->uProjectionMatrixLoc, 1, false, projectionMat);
+    glUniformMatrix4fv(shaderProgram->uNormalMatrixLoc, 1, false, normalMat);
+    
+    glUniform3f(shaderProgram->uColorLoc, material->color[0], material->color[1], material->color[2]);
+    glUniform1f(shaderProgram->uMinColorLoc, 0.2);
+    
+    Cvec3 lightPosWorld0 = Cvec3(0.0, 10.0, 0.0);
+    Cvec4 lightPosEye0 = normalMatrix(camera->getViewMatrix()) * Cvec4(lightPosWorld0, 1);
+    glUniform3f(shaderProgram->uLightPositionLoc0, lightPosEye0[0], lightPosEye0[1], lightPosEye0[2]);
+    glUniform3f(shaderProgram->uLightColorLoc0, 1, 1, 1);
+    glUniform3f(shaderProgram->uSpecularLightColorLoc0, 1, 1, 1);
+    
+//    Cvec3 lightPosWorld1 = Cvec3(-10, 5, 0);
+//    Cvec4 lightPosEye1 = normalMatrix(camera->getViewMatrix()) * Cvec4(lightPosWorld1, 1);
+//    glUniform3f(shaderProgram->uLightPositionLoc1, lightPosEye1[0], lightPosEye1[1], lightPosEye1[2]);
+//    glUniform3f(shaderProgram->uLightColorLoc1, 1, 1, 1);
+//    glUniform3f(shaderProgram->uSpecularLightColorLoc1, 1, 1, 1);
+    
+    //cubemap
+    if (material->cubemapApplied()) {
+        std::cout << "cubemap applied" << std::endl;
+        glBindTexture(GL_TEXTURE_CUBE_MAP, material->cubemap.texture);
+    }
+    
+    if (material->textures.size() > 0) {
+        glUniform1f(shaderProgram->uDiffuseTextureLoc, 0);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, material->textures[0].diffuseTexture);
+        glUniform1f(shaderProgram->uSpecularTextureLoc, 1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, material->textures[0].specularTexture);
+        glUniform1f(shaderProgram->uNormalMatrixLoc, 2);
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, material->textures[0].normalTexture);
+    }
+    
+//    for (int i = 0; i < material->textures.size(); ++i) {
+//        glUniform1f(shaderProgram->uDiffuseTextureLoc, 0);
+//        glActiveTexture(GL_TEXTURE0);
+//        glBindTexture(GL_TEXTURE_2D, material->textures[i].diffuseTexture);
+//    
+//        glUniform1f(shaderProgram->uSpecularTextureLoc, 1);
+//        glActiveTexture(GL_TEXTURE1);
+//        glBindBuffer(GL_TEXTURE_2D, material->textures[i].specularTexture);
+//    
+//        glUniform1f(shaderProgram->uNormalTextureLoc, 2);
+//        glActiveTexture(GL_TEXTURE2);
+//        glBindBuffer(GL_TEXTURE_2D, material->textures[i].normalTexture);
+//    }
+    
+    if (depthTest) {
+        glEnable(GL_DEPTH_TEST);
+    } else {
+        glDisable(GL_DEPTH_TEST);
+    }
+    
+    geometry->draw(shaderProgram->aPositionLoc, shaderProgram->aNormalLoc, shaderProgram->aTexCoordLoc, shaderProgram->aBinormalLoc, shaderProgram->aTangentLoc);
+}
+
+std::string Entity::getName() {
+    return name;
+}
+
+void Entity::setVisible(bool visible) {
+    this->visible = visible;
+}
+
+bool Entity::isVisible() const {
+    return this->visible;
+}
+
+void Entity::setDepthTest(bool enable) {
+    depthTest = enable;
+}
+
+bool Entity::depthTestEnabled() const {
+    return depthTest;
+}
+
+void Entity::setPosition(Cvec3 position) {
+    transform.setPosition(position);
+}
+
+void Entity::translate(Cvec3 translation) {
+    transform.translate(translation);
+}
+
+const Cvec3& Entity::getPosition() {
+    return transform.getPosition();
+}
+
+void Entity::setRotation(Quat rotation) {
+    transform.setRotation(rotation);
+}
+
+void Entity::rotate(Quat rotation) {
+    transform.rotate(rotation);
+}
+
+const Quat& Entity::getRotation() {
+    return transform.getRotation();
+}
+
+void Entity::setScale(Cvec3 scale) {
+    transform.setScale(scale);
+}
+
+const Cvec3& Entity::getScale() {
+    return transform.getScale();
+}
+
+const Matrix4& Entity::getModelMatrix() {
+    return transform.getModelMatrix();
+}
--- src/Entity.hpp
+++ src/Entity.hpp
@@ -10,5 +10,87 @@
 #define Entity_hpp
 
 #include <stdio.h>
+#include "Transform.h"
+#include "Geometry.hpp"
+#include "Material.h"
+#include "Camera.h"
+#include "ShaderProgram.h"
+
+/**
+ * The object/Entity to be rendered.
+ */
+class Entity {
+    
+public:
+    Transform transform;
+    Geometry* geometry;
+    Material* material;
+    
+protected:
+    
+    static int id_counter;
+    
+    std::string name;
+    
+    bool visible;
+    bool depthTest;
+    
+    GLfloat modelMat[16];
+    GLfloat modelViewMat[16];
+    GLfloat projectionMat[16];
+    GLfloat normalMat[16];
+    
+    void setName(int counter) {
+        if (name.empty()) {
+            name = "Entity" + std::to_string(counter);
+        }
+    }
+    
+public:
+    Entity* parent;
+    
+    Entity() : Entity(NULL, NULL) {}
+    
+    Entity(Geometry* geometry_, Material* material_)
+    : Entity("", geometry_, material_, NULL) {}
+    
+    Entity(std::string name_, Geometry* geometry_, Material* material_)
+    : Entity(name_, geometry_, material_, NULL) {}
+    
+    Entity(Geometry* geometry_, Material* material_, Entity* parent_)
+    : Entity("", geometry_, material_, parent_) {}
+    
+    Entity(std::string name_, Geometry* geometry_, Material* material_, Entity* parent_)
+    :name(name_), geometry(geometry_), material(material_), parent(parent_) {
+        
+        ++id_counter;
+        visible = true;
+        depthTest = true;
+        
+        setName(id_counter);
+    }
+    
+    ~Entity() {
+        delete material;
+    }
+    
+    void createMesh();
+    void draw(Camera* camera, ShaderProgram* shaderProgram);
+    std::string getName();
+    void setVisible(bool visible);
+    bool isVisible() const;
+    void setDepthTest(bool enable);
+    bool depthTestEnabled() const;
+    void setPosition(Cvec3 position);
+    void translate(Cvec3 translation);
+    const Cvec3& getPosition();
+    void setRotation(Quat rotation);
+    void rotate(Quat rotation);
+    const Quat& getRotation();
+    void setScale(Cvec3 scale);
+    const Cvec3& getScale();
+    const Matrix4& getModelMatrix();
+};
+
 
 #endif /* Entity_hpp */
